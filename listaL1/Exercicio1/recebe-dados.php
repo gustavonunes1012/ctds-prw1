871242dab45b9b4c1cf836bd100a3d17b38dada2
= <=>
2025-08-21 16:27:08 -0300

    criado logica de média ponderada

--- listaL1/Exercicio1/recebe-dados.php
+++ listaL1/Exercicio1/recebe-dados.php
@@ -13,10 +13,19 @@
  <?php
   //recebendo os dados do formulário no servidor
   $aluno = $_POST["aluno"];
+  $nota1 = $_POST["nota1"];
+  $peso1 = $_POST["peso1"];
+  $nota2 = $_POST["nota2"];
+  $peso2 = $_POST["peso2"];
+  //calculando a média ponderada  
+  $media = (($nota1 * $peso1)+($nota2 * $peso2))/($peso1 + $peso2);
+  //utilizando formatação númerica em php
+  $mediaFormatada = number_format($media, 1, ",");
  
 
-
-  echo "<p> Nome do aluno: $aluno </p>";
+echo "<p>Resultado do processamento das notas do aluno
+Aluno: $aluno
+Média: <span>$mediaFormatada</span></p>";
 
  ?>
  
